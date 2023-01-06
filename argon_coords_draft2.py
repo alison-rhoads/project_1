ef35df0066c6bbb853ec1cc209fc0057932ea0e9
alison-rhoads <121977692+alison-rhoads@users.noreply.github.com>
2023-01-06 11:39:00 -0800

    Update argon_coords_draft2.py

--- argon_coords_draft2.py
+++ argon_coords_draft2.py
@@ -1,8 +1,8 @@
 #read the initial_dat_draft2.py, print the numpy array and store atomic number
 
-data_file = open("initial_dat.py", 'r')
+data_file = open("initial_dat_draft2.py", 'r')
 
-from initial_dat_draft2.py import ngas_box
-print(ngas_box)
+from initial_dat_draft2.py import gasn_box
+print(gasn_box)
 from initial_dat_draft2.py import atomic_number
 print(atomic_number)
