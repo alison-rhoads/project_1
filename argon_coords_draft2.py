2052879198fba2b0c341e0bcd2a282946caf3d5e
alison-rhoads <121977692+alison-rhoads@users.noreply.github.com>
2023-01-06 09:42:42 -0800

    Add files via upload

---
+++ argon_coords_draft2.py
@@ -0,0 +1,8 @@
+#read the initial_dat_draft2.py, print the numpy array and store atomic number
+
+data_file = open("initial_dat.py", 'r')
+
+from initial_dat_draft2.py import ngas_box
+print(ngas_box)
+from initial_dat_draft2.py import atomic_number
+print(atomic_number)
---
+++ initial_dat_draft2.py
@@ -0,0 +1,30 @@
+import numpy as np
+import math
+
+volume = 8 #angstroms cubed
+number_of_atoms = 100
+
+atoms_cube_root = (number_of_atoms ** (1./3))
+volume_cube_root = (volume ** (1./3))
+atom_spacing = math.ceil(volume_cube_root / atoms_cube_root)
+
+#create empty 3d array
+
+ngas_box = np.empty((number_of_atoms,number_of_atoms, number_of_atoms)
+
+#create vector representing atom coordinates
+
+atom_vector = np.linspace(0, 2, number_of_atoms)
+
+#repeat and translate the vector and add to 3d array
+
+for length in range(0, 2, atom_spacing):
+    shift_vector_horiz = np.roll(atom_vector, atom_spacing, 0)
+    ngas_box = np.append(shift_vector_horiz, ngas_box, 0)
+for length in range(0, 2, atom_spacing):                                        
+    shift_vector_vert = np.roll(atom_vector, atom_spacing, 1)
+    ngas_box = np.append(shift_vector_horiz, ngas_box, 1)
+
+#create list with atomic number of atoms in array
+
+atomic_number = [18] * number_of_atoms
